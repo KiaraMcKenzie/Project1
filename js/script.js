301676480321752b3c066ac513088671dcb5c75b
Kiara McKenzie <600KiaraMcKenzie@users.noreply.github.com>
2020-10-07 20:02:12 -0500

    max change 1
    fixed font
    adding javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-
+// to validate that first name field is not blank
+function validateForm() {
+var x = document.forms["Project"]["firstname"].value;
+  if (x == "") {
+    alert("First name and last name must be filled out");
+    return false;
+  }
+ // to validate that last name field is not blank
+var x = document.forms["Project"]["lastname"].value;
+  if (x == "") {
+    alert("last name must be filled out");
+    return false;
+  }
+var x = document.forms["Project"]["lastname"].value;
+  if (x == "") {
+    alert("last name must be filled out");
+    return false;
+  }
+}
